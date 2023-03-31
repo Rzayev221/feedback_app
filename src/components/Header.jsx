2c1ca4080951663842499769e936d4653b781073
Mac <rzayev558@gmail.com>
2023-03-31 22:26:09 +0200

    Fixed Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ function Header({text, bgColor, textColor}) {
   return (
    <header style={headerStyles}>
 
-    <div className='container'></div>
-    <h2>{text}</h2>
+    <div className='container'>    <h2>{text}</h2></div>
+
 
    </header>
   )
